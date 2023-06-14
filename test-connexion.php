f5be13c65ada4de2364efafb5f2b73c37c4f84dc
Siphin <91201039+Siphin@users.noreply.github.com>
2023-06-15 00:50:46 +0200

    Tests connexion BDD

--- test-connexion.php
+++ test-connexion.php
@@ -1,19 +1,21 @@
 <?php
-$servername = "DEV1-G7-SQL-BDD";
-$username = "dev-alyqator";
-$password = "Jef!Z37;";
-$dbname = "test-database";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $servername = "DEV1-G7-SQL-BDD";
+    $username = "dev-alyqator";
+    $password = "Jef!Z37;";
+    $dbname = "test-database";
 
-// Créer une connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Créer une connexion
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérifier la connexion
-if ($conn->connect_error) {
-    echo "Echec de la connexion a la BDD : " . $conn->connect_error;
-} else {
-    echo "Connexion a la BDD reussie !";
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        $result = "Echec de la connexion a la base de donnees : " . $conn->connect_error;
+    } else {
+        $result = "Connexion a la base de donnees reussie !";
+    }
+
+    // Fermer la connexion
+    $conn->close();
 }
-
-// Fermer la connexion
-$conn->close();
 ?>
