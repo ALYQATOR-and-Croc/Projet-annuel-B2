845f1b1b6ff9c2447e59a75f41131869459d6a40
Yoann VINCENT <yvincent@gaia.lan>
2023-07-04 09:53:26 +0200

    changement couleur texte et bouton planning et création de PlanningTeacher

---
+++ frontend/src/components/Teacher/TeacherPlanning.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+//import CalendarDay from './CalendarDay';
+import CalendarMonth from '../CalendarMonth';
+import CalendarWeek from '../CalendarWeek'
+
+import '../../styles/PlanningStudent.css'
+
+export default function TeacherPlanning() {
+
+  const [SelectedDate, setSelectedDate] = useState(null);
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
+  return (
+    <div className='planning-container'>
+      <div className='planning-month'>
+        <CalendarMonth onDateClick={handleDateClick}/>
+      </div>
+      <div className='planning-week'>
+        <CalendarWeek SelectedDate={SelectedDate}/>
+      </div>
+    </div>
+  );
+}
