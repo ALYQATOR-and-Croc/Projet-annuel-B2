e34fc404659e60d0e0b86d5d6fa3e762fee0f798
Luigi <luigiaubrypouget@gmail.com>
2023-07-12 02:15:38 +0200

    continuation de l'adaptation à l'api pour l'emargement et l'affichage des cours a emarger sur le dashboard teacher

--- frontend/src/components/RegisterTable.js
+++ frontend/src/components/RegisterTable.js
@@ -3,40 +3,13 @@ import { DataGrid, frFR } from '@mui/x-data-grid';
 import '../styles/RegisterTable.css';
 
 const columns = [
-  { field: 'firstName', headerName: 'Prénom', flex: 1, hideable: false},
-  { field: 'lastName', headerName: 'Nom', flex: 1},
-  { field: 'email', headerName: 'Adresse mail', flex: 2},
-  { field: 'class', headerName: 'Classe', flex: 0.5}
-];
-
-const eleves = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', email: 'jon@snow.fr', class: 'B2 ESGI'},
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', email: 'cersei@lannister.fr', class: 'B2 ESGI'},
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', email: 'jaime@lannister.fr', class: 'B2 ESGI'},
-  { id: 4, lastName: 'Stark', firstName: 'Arya', email: 'arya@stark.fr', class: 'B2 ESGI'},
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', email: 'daenerys@targaryenmaisenpluslong.fr', class: 'B2 ESGI'},
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', email: 'ferrera@clifford.fr', class: 'B2 ESGI'},
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', email: 'rossini@frances.fr', class: 'B2 ESGI'},
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', email: 'harvey@roxie.fr', class: 'B2 ESGI'},
-  { id: 10, lastName: 'Snow', firstName: 'Jon', email: 'jon@snow.fr', class: 'B2 ESGI'},
-  { id: 12, lastName: 'Lannister', firstName: 'Cersei', email: 'cersei@lannister.fr', class: 'B2 ESGI'},
-  { id: 13, lastName: 'Lannister', firstName: 'Jaime', email: 'jaime@lannister.fr', class: 'B2 ESGI'},
-  { id: 14, lastName: 'Stark', firstName: 'Arya', email: 'arya@stark.fr', class: 'B2 ESGI'},
-  { id: 15, lastName: 'Targaryen', firstName: 'Daenerys', email: 'daenerys@targaryenmaisenpluslong.fr', class: 'B2 ESGI'},
-  { id: 17, lastName: 'Clifford', firstName: 'Ferrara', email: 'ferrera@clifford.fr', class: 'B2 ESGI'},
-  { id: 18, lastName: 'Frances', firstName: 'Rossini', email: 'rossini@frances.fr', class: 'B2 ESGI'},
-  { id: 19, lastName: 'Roxie', firstName: 'Harvey', email: 'harvey@roxie.fr', class: 'B2 ESGI'},
-  { id: 11, lastName: 'Snow', firstName: 'Jon', email: 'jon@snow.fr', class: 'B2 ESGI'},
-  { id: 22, lastName: 'Lannister', firstName: 'Cersei', email: 'cersei@lannister.fr', class: 'B2 ESGI'},
-  { id: 33, lastName: 'Lannister', firstName: 'Jaime', email: 'jaime@lannister.fr', class: 'B2 ESGI'},
-  { id: 44, lastName: 'Stark', firstName: 'Arya', email: 'arya@stark.fr', class: 'B2 ESGI'},
-  { id: 65, lastName: 'Targaryen', firstName: 'Daenerys', email: 'daenerys@targaryenmaisenpluslong.fr', class: 'B2 ESGI'},
-  { id: 77, lastName: 'Clifford', firstName: 'Ferrara', email: 'ferrera@clifford.fr', class: 'B2 ESGI'},
-  { id: 84, lastName: 'Frances', firstName: 'Rossini', email: 'rossini@frances.fr', class: 'B2 ESGI'},
-  { id: 39, lastName: 'Roxie', firstName: 'Harvey', email: 'harvey@roxie.fr', class: 'B2 ESGI'}
+  { field: 'prenom', headerName: 'Prénom', flex: 1, hideable: false},
+  { field: 'nom', headerName: 'Nom', flex: 1},
+  { field: 'adresse_email', headerName: 'Adresse mail', flex: 2},
 ];
 
 export default function RegisterTable(props) {
+  
   return (
     <div className="RegisterTable">
       <DataGrid
@@ -50,11 +23,11 @@ export default function RegisterTable(props) {
                 ? `${count.toLocaleString()} élèves sélectionnés`
                 : `${count.toLocaleString()} élève sélectionné`,
         }}
-        rows={eleves}
+        rows={props.eleves}
         columns={columns}
         initialState={{
             pagination: {
-              paginationModel: { page: 0, pageSize: eleves.length }
+              paginationModel: { page: 0, pageSize: 20 }
             },
         }}
         checkboxSelection
--- frontend/src/components/Teacher/TeacherDashboard.js
+++ frontend/src/components/Teacher/TeacherDashboard.js
@@ -10,6 +10,7 @@ import '../../styles/TeacherDashboard.css';
 
 export default function Dashboard(props) {
   const [monthPlanning, setMonthPlanning] = useState([]);
+  const [absenceDelays, setAbsenceDelays] = useState([]);
 
   const requestCalendar = (idUser) => {
     calendarService.month(props.idUser)
@@ -26,12 +27,69 @@ export default function Dashboard(props) {
     requestCalendar(props.idUser);
   }
 
-  const absenceDelays = [
-  {id:1, date: "06/07/2023", heure: "8h-9h30", matiere: "Algorithmie", classe:"B2 ESGI", Emarger:false},
-  {id:2, date: "02/06/2023", heure: "14h-15h30", matiere: "Algorithmie", classe:"B2 ESGI", Emarger:true},
-  {id:3, date: "18/02/2023", heure: "8h-9h30", matiere: "Mathématiques", classe:"M1 ESGI",Emarger:true},
-  {id:4, date: "18/02/2023", heure: "8h-9h30", matiere: "Big data", classe:"M2 ESGI", Emarger:true}
-  ];
+  if (monthPlanning.length !== 0 && absenceDelays.length === 0) {
+    let todayCourses = monthPlanning.filter((cours)=>{
+      let actualDay = new Date();
+      actualDay.setHours(actualDay.getHours() + 2);
+      actualDay = actualDay.toISOString();
+      let courseDay = cours.date_cours;
+      if (actualDay.substring(0,10) === courseDay.substring(0,10)) {
+        return true
+      } else {
+        return false
+      }
+    })
+    let absenceDelayRaw = todayCourses.map(cours => {
+      let heureCours = cours.heure_debut_cours.substring(11, 16).replace(':', 'h');
+      heureCours += '-' + cours.heure_fin_cours.substring(11, 16).replace(':', 'h');
+      let dateCours = new Date();
+      dateCours = dateCours.toLocaleDateString("fr");
+      return {
+        id: cours.id_cours, 
+        date: dateCours, 
+        heure: heureCours,
+        matiere: cours.libelle_matiere.toUpperCase(),
+        classe: cours.libelle_classe.toUpperCase(),
+        Emarger: false
+      }
+    });
+    absenceDelayRaw.sort((a, b) =>{
+      return parseInt(a.heure.substring(0, 2)) - parseInt(b.heure.substring(0, 2));
+    })
+    setAbsenceDelays(absenceDelayRaw.slice(0,3));
+  }
+
+  // if (absenceDelays.length > 0 ) {
+  //   console.log('fin', absenceDelays);
+  // }
+
+  // const fillAbsenceDelays = (monthPlanning) => {
+  //   setAbsenceDelays(monthPlanning.map(cours => {
+  //     return cours.id_cours
+  //   }));
+    
+  // }
+    // let actualDay = new Date().toISOString();
+    // let courseDay = cours.date_cours;
+    // console.log(actualDay.substring(0,10), courseDay.substring(0,10));
+    // if (new Date().toISOString().substring(0,10) === cours.date_cours.substring(0,10)) {
+      // let heureCours = cours.heure_debut_cours.substring(11, 16).replace(':', 'h');
+      // heureCours += '-' + cours.heure_fin_cours.substring(11, 16).replace(':', 'h');
+      // let dateCours = new Date();
+      // dateCours = dateCours.toLocaleDateString("fr");
+      // return (new Date().toISOString().substring(0,10) === cours.date_cours.substring(0,10)) ? {
+      // id: cours.id_cours, 
+      // date: new Date.now().toLocaleDateString("fr"), 
+      // heure: cours.heure_debut_cours.substring(11, 16).replace(':', 'h')
+      // + '-' + cours.heure_fin_cours.substring(11, 16).replace(':', 'h'),
+      // matiere: cours.libelle_matiere.toUpperCase(),
+      // classe: cours.libelle_classe.toUpperCase(),
+      // Emarger: true
+      // } : ['test']
+    // } else {
+      // return([]);
+    // }
+  // }))}
 
   return (
     <div className="TeacherDashboard">
@@ -44,9 +102,9 @@ export default function Dashboard(props) {
               </h1>
               <div className='SeeMoreButton'><Button href='/teacher/register-students' variant="contained" startIcon={<VisibilityIcon/>}>VOIR TOUT</Button></div>
             </div>
-            <h2 className='registerStudentsDashSubTitle'>Prochain cours à émarger</h2>
+            <h2 className='registerStudentsDashSubTitle'>Cours à émarger aujourd'hui</h2>
             {/* prendre les dernières absences/retards si le tri est par date la plus ancienne d'abord. */}
-            <RegisterStudentsGrid data={absenceDelays.slice(0,3)} mailap="mail.ap@esgi.fr" type="émargment"/>
+            <RegisterStudentsGrid data={absenceDelays} mailap="mail.ap@esgi.fr" type="émargment"/>
           </div>
           <Divider orientation="vertical" flexItem></Divider>
           <div className='planningDash'>
