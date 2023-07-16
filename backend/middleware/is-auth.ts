b202f047b779650aaa7fe907878d572f048a8b16
arthur-herve <98807046+arthur-herve@users.noreply.github.com>
2023-07-16 18:39:41 +0200

    feat(code) token ajout de l'id de la personne dans sa fonction

--- backend/middleware/is-auth.ts
+++ backend/middleware/is-auth.ts
@@ -25,28 +25,4 @@ const isAuthenticated = (
   }
 };
 
-const isCourseManager = (
-  request: express.Request,
-  response: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const authHeader = request.get('Authorization');
-    const token = authHeader!.replace('Bearer ', '');
-    const decodedToken: any = jwt.verify(token, secretPass.passwordToken);
-    if (
-      decodedToken.aud !== 'admin' ||
-      decodedToken.aud !== 'respPedago' ||
-      decodedToken.aud !== 'attProm' ||
-      decodedToken.aud !== 'repro'
-    ) {
-      throw new Error('Not authorized');
-    }
-    next();
-  } catch (error) {
-    console.log(error);
-    response.status(401).send(error);
-  }
-};
-
 export = isAuthenticated;
