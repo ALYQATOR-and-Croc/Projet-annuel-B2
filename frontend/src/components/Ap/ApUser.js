8ba250630ff36d4a98f0656412de3b6f5f153d81
unknown <luigiaubrypouget@gmail.com>
2023-07-17 21:11:24 +0200

    Formulaire d'AJOUT infra fonctionnel avec API

--- frontend/src/components/Ap/ApUser.js
+++ frontend/src/components/Ap/ApUser.js
@@ -86,6 +86,7 @@ const ApUser = () => {
         <FormControl fullWidth style={{ marginBottom: '20px' }}>
           <InputLabel>Classe</InputLabel>
           <Select
+            label="Classe"
             name="idClasse"
             value={formData.idClasse}
             onChange={handleChange}
@@ -113,6 +114,7 @@ const ApUser = () => {
       <FormControl fullWidth style={{ marginBottom: '20px' }}>
         <InputLabel>Fonction</InputLabel>
         <Select
+          label='Fonction'
           name="idRole"
           value={formData.idRole}
           onChange={handleChange}
