f91ed9f8c095b2766af83dac90308625a0c5ef54
arthur-herve <98807046+arthur-herve@users.noreply.github.com>
2023-07-17 21:06:09 +0200

    changement des droits routes campus et salles

--- backend/routes/infrastructure/room-route.ts
+++ backend/routes/infrastructure/room-route.ts
@@ -1,36 +1,29 @@
-import express from 'express';
-import isAuthenticated from '../../middleware/is-auth';
-import { isAdmin, isEducationManager } from '../../middleware/roles-middleware';
-import {} from '../../controllers/users/roles-controller';
+import express from "express";
+import isAuthenticated from "../../middleware/is-auth";
+import { isAdmin, isEducationManager } from "../../middleware/roles-middleware";
+import {} from "../../controllers/users/roles-controller";
 import {
   newRoomPOST,
   getRoomsGET,
   getRoomsByCampusGET,
   patchRoomPATCH,
-} from '../../controllers/infrastructure/room-controller';
+} from "../../controllers/infrastructure/room-controller";
 
 const router = express.Router();
 
-router.post(
-  '/room/new/',
-  isAuthenticated,
-  isAdmin,
-  isEducationManager,
-  newRoomPOST
-);
+router.post("/room/new/", isAuthenticated, isEducationManager, newRoomPOST);
 
-router.get('/rooms/', isAuthenticated, isEducationManager, getRoomsGET);
+router.get("/rooms/", isAuthenticated, isEducationManager, getRoomsGET);
 
 router.patch(
-  '/room/:idRoom/',
+  "/room/:idRoom/",
   isAuthenticated,
-  isAdmin,
   isEducationManager,
   patchRoomPATCH
 );
 
 router.get(
-  '/rooms/campus/:idCampus/',
+  "/rooms/campus/:idCampus/",
   isAuthenticated,
   isEducationManager,
   getRoomsByCampusGET
